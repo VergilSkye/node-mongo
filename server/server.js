fb5555613e632540b0bb03f9ed023a07d659cc9a
VergilSkye <virgiliosps@gmail.com>
2018-07-22 13:13:41 -0300

    Add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,41 +1,72 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+const {
+    mongoose
+} = require('./db/mongoose');
+const {
+    ObjectID
+} = require('mongodb');
+
+const {
+    Todo
+} = require('./models/todo');
+const {
+    User
+} = require('./models/user');
 
 var app = express();
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{  
+app.post('/todos', (req, res) => {
     var todo = new Todo({
-        text:req.body.text
+        text: req.body.text
     });
 
-    todo.save().then((doc)=> {
+    todo.save().then((doc) => {
         res.send(doc);
-    }, (e)=>{
+    }, (e) => {
         res.status(400).send(e);
-        
+
     });
-      
+
 });
-app.get('/todos',(req,res)=>{
+app.get('/todos', (req, res) => {
     Todo.find().then((Todo) => {
-        res.send({Todo});
-    },(e)=>{
+        res.send({
+            Todo
+        });
+    }, (e) => {
         res.status(400).send(e);
     });
 });
 
 
 
+//Get /todos/1234324
 
-
-app.listen(3000,()=>{
-    console.log('Started on port 3000');    
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+     if (!ObjectID.isValid(id)) {
+         return res.status(404).send();
+     }
+    Todo.findById(id).then((Todo) => {  
+        if(!Todo){
+            res.status(404).send();
+        } 
+        res.send({Todo});
+    },  (e) => {
+        res.status(400).send(e);
+    });
 });
 
 
-module.exports = {app};
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
+
+
+module.exports = {
+    app
+};
